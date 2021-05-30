7712bf3ca2c6b0264f331705aca600506a173067
João Bispo <joaobispo2077@gmail.com>
2021-05-30 15:00:41 -0300

    feat: config base app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import express from 'express';
+import routes from './routes';
 
 class AppController {
-  private _express = express();
-  constructor() {}
+  public app = express();
+
+  constructor() {
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  routes() {
+    this.app.use(routes);
+  }
 }
+
+export default new AppController().app;
