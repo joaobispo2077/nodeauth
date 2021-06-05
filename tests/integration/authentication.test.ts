cd0d63931c7413033963e9c38db3985bb7b87e34
João Bispo <joaobispo2077@gmail.com>
2021-06-05 12:25:41 -0300

    test: should not authenticate with invalid credentials

--- tests/integration/authentication.test.ts
+++ tests/integration/authentication.test.ts
@@ -3,6 +3,17 @@ import app from '../../src/app';
 
 import connection from '../../src/database/connection';
 
+const createMockUser = async () => {
+  const userToCreate = {
+    email: 'test@gmail.com',
+    name: 'Testing',
+    password: '123456',
+  };
+
+  const response = await request(app).post('/users').send(userToCreate);
+  return response.body;
+};
+
 describe('Authentication', () => {
   beforeAll(async () => {
     const connectionDB = await connection.create();
@@ -13,19 +24,36 @@ describe('Authentication', () => {
     await connection.close();
   });
 
-  beforeEach(async () => {
+  beforeEach(() => async () => {
     await connection.clear();
   });
 
   it('should receive JWT token when authenticated with valid credentials', async () => {
     const user = {
-      email: 'teste@gmail.com',
-      password_hash: '123456',
+      email: 'test@gmail.com',
+      password: '123456',
     };
 
     const response = await request(app).post('/auth').send(user);
 
     expect(response.status).toBe(200);
+    expect(1).toBe(1);
+  });
+
+  it('should not authenticate with invalid credentials', async () => {
+    const userMock = await createMockUser();
+
+    const userMockWithInvalidPassword = Object.assign({}, userMock, {
+      password: '00000000000000000',
+    });
+
+    console.log(userMockWithInvalidPassword);
+
+    const response = await request(app)
+      .post('/auth')
+      .send(userMockWithInvalidPassword);
+
+    expect(response.status).toBe(401);
     expect(1).toBe(1);
   });
 });
