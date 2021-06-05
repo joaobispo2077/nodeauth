3e4b29123f78ad3087bc722510bb2d3a5ce54df0
João Bispo <joaobispo2077@gmail.com>
2021-06-05 14:01:25 -0300

    feat: config coverage reports

--- jest.config.js
+++ jest.config.js
@@ -17,13 +17,17 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    'src/**',
+    '!src/interfaces/**',
+    '!src/database/migrations/**',
+  ],
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+  coverageDirectory: './tests/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
